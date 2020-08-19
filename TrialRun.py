e76dc4e498629d8d4f78f046890a4ea7bda044bf
Ngô Văn Khôi <46133150+khoingo123@users.noreply.github.com>
2020-08-19 16:17:26 +0700

    Delete TrialRun.py

--- TrialRun.py
+++
@@ -1,5 +0,0 @@
-import pygame
-pygame.init()
-pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
-print(width, height)
